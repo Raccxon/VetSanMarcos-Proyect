36a12cf72d1b17b9a7d0ce5d350e1c69d508f1db
AuDrakz <ala.ojeda@duocuc.cl>
2026-09-25 19:39:23 -0300

    fix: Arreglo de contenido mezclado entre main.jsx y App.jsx y typo StricMode, tambien inclusion de navegacion a Servicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-// corrector ortografico
-import { StricMode } from 'react'
-// trae funcion que conecta react con el HTML
-import { createRoot } from 'react-dom/client'
-// trae el componente que activa las rutas de la aplicacion
-import { BrowserRouter } from 'react-router-dom'
-// trae el archivo de estilos de bootstrap (CSS)
-import  'bootstrap/dist/css/bootstrap.min.css'
-// trae nuestros estilos
-import './index.css'
-// trae el componente de toda la aplicacion
-import App from './App.jsx'
+import { Routes, Route, Link } from 'react-router-dom'
+import ServiciosPage from './pages/ServiciosPage'
 
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Inicio</Link>
+        {' | '}
+        <Link to="/servicios">Servicios</Link>
+      </nav>
 
-createRoot(document.getElementById('root')).render(
-  <StricMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StricMode>
-)
+      <Routes>
+        <Route path="/" element={<h1>Veterinaria San Marcos</h1>} />
+        <Route path="/servicios" element={<ServiciosPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+// corrector ortografico
 import { StrictMode } from 'react'
+// trae funcion que conecta react con el HTML
 import { createRoot } from 'react-dom/client'
+// trae el componente que activa las rutas de la aplicacion
+import { BrowserRouter } from 'react-router-dom'
+// trae el archivo de estilos de bootstrap (CSS)
+import  'bootstrap/dist/css/bootstrap.min.css'
+// trae nuestros estilos
 import './index.css'
+// trae el componente de toda la aplicacion
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
